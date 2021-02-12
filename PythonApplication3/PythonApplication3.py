31ecffe7880d16f574c1afac2fc38c02f82bf842
John Hurt <rita.uy56@gmail.com>
2021-02-12 12:30:26 +1100

    Update1

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -16,26 +16,12 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
 
-# Check the versions of libraries
-
-# Python version
-import sys
-print('Python: {}'.format(sys.version))
-# scipy
-import scipy
-print('scipy: {}'.format(scipy.__version__))
-# numpy
-import numpy
-print('numpy: {}'.format(numpy.__version__))
-# matplotlib
-import matplotlib
-print('matplotlib: {}'.format(matplotlib.__version__))
-# pandas
-import pandas
-print('pandas: {}'.format(pandas.__version__))
-# scikit-learn
-import sklearn
-print('sklearn: {}'.format(sklearn.__version__))
-
+print(dataset.shape)
+print(dataset.head(20))
+print(dataset.describe())
+print(dataset.groupby('class').size())
 
